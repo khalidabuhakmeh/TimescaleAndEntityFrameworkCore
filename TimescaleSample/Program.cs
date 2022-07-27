2287bb92ede33765c64fd16d50ff08f2e67dbd3c
Khalid Abuhakmeh <khalidabuhakmeh@gmail.com>
2022-07-27 12:58:42 -0400

    moved some things around

--- TimescaleSample/Program.cs
+++ TimescaleSample/Program.cs
@@ -3,18 +3,18 @@ using TimescaleSample.Models;
 
 var db = new StocksDbContext();
 
-// UTC Only
-// Read this and cry https://www.npgsql.org/doc/types/datetime.html
-var date = new DateTime(2022, 06, 29, 0, 0, 0, DateTimeKind.Utc);
-FormattableString sql = $"""
+var sql = """
 SELECT * FROM "Stocks"
 WHERE "Time" > now() - INTERVAL '1 week'
 AND "Symbol" = 'MSFT'
 """ ;
 
-var trades = db.Stocks.FromSqlInterpolated(sql).Count();
+var trades = db.Stocks.FromSqlRaw(sql).Count();
 Console.WriteLine($"{trades} trades of MSFT in the last week");
 
+// UTC Only
+// Read this and cry https://www.npgsql.org/doc/types/datetime.html
+var date = new DateTime(2022, 06, 29, 0, 0, 0, DateTimeKind.Utc);
 var top = db
     .GetWeeklyResults(date)
     .First(x => x.Symbol == "MSFT");
